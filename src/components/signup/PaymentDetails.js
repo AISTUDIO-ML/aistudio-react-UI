a66ce41265033e79da0a61d59c74bfa7a1f8040c
himanshu5455 <himanshulinux01@gmail.com>
2023-02-10 17:34:05 +0530

    backend latest10.02

--- src/components/signup/PaymentDetails.js
+++ src/components/signup/PaymentDetails.js
@@ -1,61 +1,53 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import card from "../assets/images/card.png";
-import { Link ,useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "../header/Header";
 import { useFormik } from "formik";
 import { PaymentDetailsSchema } from "./PaymentDetailsSchema";
 import { useCreditCardValidator, images } from "react-creditcard-validator";
 import $ from "jquery";
 import { UpdateUserpay } from "../service";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import moment from 'moment';
+import toast from 'react-hot-toast';
 
 
 function PaymentDetails() {
   const navigate = useNavigate();
-  
-// 
-const[cvc,setCvc]=useState();
-const[cardNum,setCardNum]=useState();
-const[expiry,setExpiry]=useState();
-const[namee,setName]=useState();
 
-var card_number = document.getElementById('card_number');
+  // 
+  const [cvc, setCvc] = useState();
+  const [cardNum, setCardNum] = useState();
+  const [expiry, setExpiry] = useState();
+
+  var card_number = document.getElementById('card_number');
 
   // Removing white space through jquery
-let data = 
-  useEffect(() => {
-    // $("input#space").on({
-    //   keydown: function (e) {
-    //     if (e.which === 32) return false;
-    //   },
-    //   change: function () {
-    //     this.value = this.value.replace(/\s/g, "");
-    //   },
-    // });
-    $('#space').on('keydown', function(e) {
-      console.log(this.value);
-      if( e.which === 32 && this.value === '' ) {
-        return false;
-      }
-    });
-  }, []);
+  let data =
+    useEffect(() => {
+
+      $('#space').on('keydown', function (e) {
+        console.log(this.value);
+        if (e.which === 32 && this.value === '') {
+          return false;
+        }
+      });
+    }, []);
 
   const {
-    getCardNumberProps, getExpiryDateProps, 
-    getCVCProps,getCardImageProps, meta: { erroredInputs },
-    
+    getCardNumberProps, getExpiryDateProps,
+    getCVCProps, getCardImageProps, meta: { erroredInputs },
+
   } = useCreditCardValidator();
 
-const handleChangeCardNumber=(e)=>{
-setCardNum(e.target.value)
-}
-const handleChangeExpiry=(e)=>{
-  setExpiry(e.target.value)
+  const handleChangeCardNumber = (e) => {
+    setCardNum(e.target.value)
   }
-const handleChangeCvc=(e)=>{
+  const handleChangeExpiry = (e) => {
+    setExpiry(e.target.value)
+  }
+  const handleChangeCvc = (e) => {
     setCvc(e.target.value)
-    }
+  }
   // using formik
   const formInitialValues = {
     cardholder: "",
@@ -64,6 +56,7 @@ const handleChangeCvc=(e)=>{
     cvc: "",
   };
   // using formik
+
   const { handleChange, handleBlur, touched, values, errors } =
     useFormik({
       initialValues: formInitialValues,
@@ -71,34 +64,28 @@ const handleChangeCvc=(e)=>{
 
       onSubmit: (values) => {
         console.log(values);
-        // console.log(namee);
-        // console.log(cardNum);
-        // console.log(cvc);
-        // console.log(expiry);
       },
     });
-const handleSubmit = (e) =>{
-  e.preventDefault();
-  UpdateUserpay(values.cardholder,cardNum,cvc,expiry).then(response => {
-    toast.success("Account Created !!", {
-      position:toast.POSITION.TOP_CENTER,
-      autoClose:1500 
-      
-    });
-    navigate('/main')
-    // 
-   
- 
-  })
-    .catch(error =>{
-      console.log('An error occurred:',error.response.data);
-    });
-
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let datee = moment(expiry, 'MM/YY').format()
+    if (erroredInputs.cardNumber != 'Card number is invalid') {
+      if (values.cardholder && cardNum && cvc && datee) {
+        UpdateUserpay(values.cardholder, cardNum, cvc, datee).then(response => {
+          toast.success('Account Created !!', {
+            duration: 2000,
+            position: 'top-center'})
+          navigate('/main')
+        })
+          .catch(error => {
+            console.log('An error occurred:', error.response.data);
+          });
+      }
+    }
+  }
   return (
     <>
       <Header />
-      <ToastContainer autoClose={7000}/>
       <section className="main">
         <div className="row">
           <div className="col">
@@ -115,7 +102,7 @@ const handleSubmit = (e) =>{
                     className="form-control"
                     placeholder="Type Here"
                     name="cardholder"
-          
+
                     value={values.cardholder}
                     onChange={handleChange}
                     onBlur={handleBlur}
@@ -136,16 +123,16 @@ const handleSubmit = (e) =>{
                       className="form-control"
                       placeholder="Type Here"
                       id="card_number"
-                      {...getCardNumberProps({onChange: handleChangeCardNumber })}
+                      {...getCardNumberProps({ onChange: handleChangeCardNumber })}
                     />
                     <svg className="card_img"{...getCardImageProps({ images })} />
                   </div>
                   <small className="card_validation" >{erroredInputs.cardNumber && erroredInputs.cardNumber}</small>
 
-                 
+
                 </div>
 
-                  {/*  Expiry Date */}
+                {/*  Expiry Date */}
                 <div className="form-row">
                   <div className="col-md-6 mb-3">
                     <label>Expiry date</label>
@@ -153,25 +140,25 @@ const handleSubmit = (e) =>{
                       type="text"
                       className="form-control"
                       placeholder="MM/DD/YY"
-                      {...getExpiryDateProps({onChange: handleChangeExpiry })}
+                      {...getExpiryDateProps({ onChange: handleChangeExpiry })}
                     />
 
                     <small className="card_validation">{erroredInputs.expiryDate && erroredInputs.expiryDate}</small>
-                 
+
                   </div>
 
 
-                    {/*  CVC */}
+                  {/*  CVC */}
                   <div className="col-md-6 mb-3">
                     <label>CVC</label>
                     <input
                       type="text"
                       className="form-control"
                       placeholder="Ex. 1234"
-                       {...getCVCProps({onChange: handleChangeCvc })}
+                      {...getCVCProps({ onChange: handleChangeCvc })}
                     />
                     <small className="card_validation">{erroredInputs.cvc && erroredInputs.cvc}</small>
-                  
+
                   </div>
                 </div>
                 <div className="explore">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Toaster } from 'react-hot-toast';
+
 // 1. import `SaasProvider` component. This wraps the `ChakraProvider` component.
 // import { SaasProvider } from '@saas-ui/react'
 
@@ -8,8 +10,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-  <App />
+ <> <Toaster  />
+ <App /></>
 
 );
 
