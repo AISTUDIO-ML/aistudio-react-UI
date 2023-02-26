5449bc501cea82dc52511a85dea38b6c6661bab8
WaxifAlee <waxif.alee1@gmail.com>
2023-02-27 00:35:15 +0500

    Added the side navigation menu in the dashboard page. Also made the code more readable by fixing the import syntax in App.js

--- src/App.js
+++ src/App.js
@@ -1,91 +1,89 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/jquery/dist/jquery.slim.min.js";
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js"; 
-import "./components/style.css";
-import Login from "./components/login/Login";
-import Header from "./components/header/Header";
-import SignUp from "./components/signup/SignUp";
-import MoreSteps from "./components/signup/MoreSteps";
-import PaymentDetails from "./components/signup/PaymentDetails";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Hosting from "./components/hosting/Hosting";
-import Main from "./components/main/Main";
-import DataConnection from "./components/data_connection/dataConnection";
-import SelectDataStore from "./components/data_connection/selectDataStore";
-import SelectDataUrl from "./components/data_connection/selectDataUrl";
-import SelectAdvOption from "./components/data_connection/showAdvOpt";
-import Advanceoption01 from "./components/advance-options/advanceoption01";
-import Advanceoption02 from "./components/advance-options/advanceoption02";
-import Advanceoption03 from "./components/advance-options/advanceoption03";
-import Advanceoption04 from "./components/advance-options/advanceoption04";
-import OverView from "./components/overview/overview";
-import OverView01 from "./components/overview/overview01";
-import OverView02 from "./components/overview/overview02";
-import Statistics from "./components/statistics/statistics";
-import StatisticsExpand from "./components/statistics/statistics_expand";
-import ProjectUpdates from "./components/project-updates/updates";
-import Experiment01 from "./components/experiments/Experiment01";
-import Experiment02 from "./components/experiments/Experiment02";
-import Experiment03 from "./components/experiments/Experiment03";
-import Model01 from "./components/model/Model01";
-import Model02 from "./components/model/Model02";
-import Model03 from "./components/model/Model03";
-import ProjectDetails from "./components/project-details/ProjectDetails";
-import MainLearnMore from "./components/data_connection/main-learn-more.component";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
+import "./components/style.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Dashboard } from "./pages";
+
+import {
+  Login,
+  Header,
+  SignUp,
+  MoreSteps,
+  PaymentDetails,
+  Hosting,
+  Main,
+  DataConnection,
+  SelectDataStore,
+  SelectDataUrl,
+  ShowAdvOpt,
+  Advanceoption01,
+  SelectAdvOption,
+  MainLearnMore,
+  Statistics,
+  StatisticsExpand,
+  Advanceoption02,
+  Advanceoption03,
+  Advanceoption04,
+  ProjectUpdates,
+  ProjectDetails,
+  OverView,
+  OverView01,
+  OverView02,
+  Experiment01,
+  Experiment02,
+  Experiment03,
+  Model01,
+  Model02,
+  Model03,
+} from "./components";
 
 function App() {
-
-
-
-
   return (
     <>
-    
-    <div class="container">
-      <BrowserRouter>        
+      <div class="container">
+        <BrowserRouter>
           <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/moreSteps" element={<MoreSteps />} />
-          <Route path="/paymentDetails" element={<PaymentDetails />} />
-          <Route path="/hosting" element={<Hosting />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/dataConnection" element={<DataConnection />} />
-          <Route path="/selectDataStore" element={<SelectDataStore />} />
-          <Route path="/selectDataUrl" element={<SelectDataUrl/>} />
-          <Route path="/showAdvOption" element={<SelectAdvOption/>} />
-          <Route path="/mainLearnMore" element={<MainLearnMore/>} />
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/moreSteps" element={<MoreSteps />} />
+            <Route path="/paymentDetails" element={<PaymentDetails />} />
+            <Route path="/hosting" element={<Hosting />} />
+            <Route path="/main" element={<Main />} />
 
-          <Route path="/statistics" element={<Statistics/>} />
-          <Route path="/statistics_expand" element={<StatisticsExpand/>} />
+            <Route path="/dashboard" element={<Dashboard />} />
 
-          <Route path="/advanceoption01" element={<Advanceoption01/>} />
-          <Route path="/advanceoption02" element={<Advanceoption02/>} />
-          <Route path="/advanceoption03" element={<Advanceoption03/>} />
-          <Route path="/advanceoption04" element={<Advanceoption04/>} />
+            <Route path="/dataConnection" element={<DataConnection />} />
+            <Route path="/selectDataStore" element={<SelectDataStore />} />
+            <Route path="/selectDataUrl" element={<SelectDataUrl />} />
+            <Route path="/showAdvOption" element={<SelectAdvOption />} />
+            <Route path="/mainLearnMore" element={<MainLearnMore />} />
 
-          <Route path="/projects_update" element={<ProjectUpdates/>} />
-          <Route path="/projectdetails" element={<ProjectDetails />} />
+            <Route path="/statistics" element={<Statistics />} />
+            <Route path="/statistics_expand" element={<StatisticsExpand />} />
 
-          <Route path="/overview" element={<OverView/>} />
-          <Route path="/overview01" element={<OverView01/>} />
-          <Route path="/overview02" element={<OverView02/>} />
+            <Route path="/advanceoption01" element={<Advanceoption01 />} />
+            <Route path="/advanceoption02" element={<Advanceoption02 />} />
+            <Route path="/advanceoption03" element={<Advanceoption03 />} />
+            <Route path="/advanceoption04" element={<Advanceoption04 />} />
 
+            <Route path="/projects_update" element={<ProjectUpdates />} />
+            <Route path="/projectdetails" element={<ProjectDetails />} />
 
-          <Route path="/experiment01" element={<Experiment01 />} />
-          <Route path="/experiment02" element={<Experiment02 />} />
-          <Route path="/experiment03" element={<Experiment03 />} />
-          <Route path="/model01" element={<Model01 />} />
-          <Route path="/model02" element={<Model02 />} />
-          <Route path="/model03" element={<Model03 />} />
+            <Route path="/overview" element={<OverView />} />
+            <Route path="/overview01" element={<OverView01 />} />
+            <Route path="/overview02" element={<OverView02 />} />
 
-
-
-
-
-        </Routes>
-      </BrowserRouter>
+            <Route path="/experiment01" element={<Experiment01 />} />
+            <Route path="/experiment02" element={<Experiment02 />} />
+            <Route path="/experiment03" element={<Experiment03 />} />
+            <Route path="/model01" element={<Model01 />} />
+            <Route path="/model02" element={<Model02 />} />
+            <Route path="/model03" element={<Model03 />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
---
+++ src/components/index.js
@@ -0,0 +1,47 @@
+export { default as Advanceoption01 } from "./advance-options/advanceoption01";
+export { default as Advanceoption02 } from "./advance-options/advanceoption02";
+export { default as Advanceoption03 } from "./advance-options/advanceoption03";
+export { default as Advanceoption04 } from "./advance-options/advanceoption04";
+
+export { default as DataConnection } from "./data_connection/dataConnection";
+export { default as SelectDataStore } from "./data_connection/selectDataStore";
+export { default as SelectDataUrl } from "./data_connection/selectDataUrl";
+export { default as ShowAdvOpt } from "./data_connection/showAdvOpt";
+export { default as SelectAdvOption } from "./data_connection/showAdvOpt";
+export { default as MainLearnMore } from "./data_connection/main-learn-more.component";
+
+export { default as Header } from "./header/Header";
+export { default as MainHeader } from "./header/MainHeader";
+
+export { default as Experiment01 } from "./experiments/Experiment01";
+export { default as Experiment02 } from "./experiments/Experiment02";
+export { default as Experiment03 } from "./experiments/Experiment03";
+
+export { default as Hosting } from "./hosting/Hosting";
+
+export { default as Login } from "./login/Login";
+// export { default as LoginFormSchema } from "./login/LoginFormSchema";
+
+export { default as Main } from "./main/Main";
+
+export { default as Model01 } from "./model/Model01";
+export { default as Model02 } from "./model/Model02";
+export { default as Model03 } from "./model/Model03";
+
+export { default as OverView } from "./overview/overview";
+export { default as OverView01 } from "./overview/overview01";
+export { default as OverView02 } from "./overview/overview02";
+
+export { default as ProjectDetails } from "./project-details/ProjectDetails";
+
+export { default as ProjectUpdates } from "./project-updates/updates";
+
+export { default as MoreSteps } from "./signup/MoreSteps.js";
+// export {default as MoreStepSchema} from "./signup/MoreStepSchema";
+export { default as PaymentDetails } from "./signup/PaymentDetails";
+// export {default as PaymentDetailsSchema} from "./signup/PaymentDetailsSchema";
+export { default as SignUp } from "./signup/SignUp";
+// export {default as SignUpFormSchema} from "./signup/SignUpFormSchema";
+
+export { default as Statistics } from "./statistics/statistics";
+export { default as StatisticsExpand } from "./statistics/statistics_expand";
